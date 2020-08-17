aff9911c34fb2355a36e1e94c6f864423cc176e9
sudeepthinittala <17pa1a05b5@vishnu.edu.in>
2020-08-17 18:28:51 +0530

    Updates

---
+++ precache-manifest.d050765e366248d11ed7b2caa812b471.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202ada4a2c4298184227b16ea7df8435",
+    "url": "/online-quiz/index.html"
+  },
+  {
+    "revision": "567e32953c4fe5fbccdf",
+    "url": "/online-quiz/static/css/2.a2822f15.chunk.css"
+  },
+  {
+    "revision": "a77f62d06203f2886db1",
+    "url": "/online-quiz/static/css/main.d1b11cd0.chunk.css"
+  },
+  {
+    "revision": "567e32953c4fe5fbccdf",
+    "url": "/online-quiz/static/js/2.36dcf902.chunk.js"
+  },
+  {
+    "revision": "7979eeaf519f92e9fad084e05c681841",
+    "url": "/online-quiz/static/js/2.36dcf902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a77f62d06203f2886db1",
+    "url": "/online-quiz/static/js/main.5b11708e.chunk.js"
+  },
+  {
+    "revision": "4fb17e322d0a94e0eae2",
+    "url": "/online-quiz/static/js/runtime-main.eb4e7794.js"
+  },
+  {
+    "revision": "77265175e11b6d5c36eeb9db52d7508d",
+    "url": "/online-quiz/static/media/Screenshot (162).77265175.png"
+  },
+  {
+    "revision": "141d3db5e9cff02f3552906384c673e2",
+    "url": "/online-quiz/static/media/Screenshot (163).141d3db5.png"
+  },
+  {
+    "revision": "82f90a6f952ab32684d3c1dcb5267b70",
+    "url": "/online-quiz/static/media/Screenshot (164).82f90a6f.png"
+  },
+  {
+    "revision": "0701dc65b59a62030c9d16c21129f08a",
+    "url": "/online-quiz/static/media/Screenshot (165).0701dc65.png"
+  },
+  {
+    "revision": "080a62a8e0f0a7e1c04f58418fe87a35",
+    "url": "/online-quiz/static/media/Screenshot (166).080a62a8.png"
+  },
+  {
+    "revision": "7f4ca6711674233dad57d0f73a0cce3e",
+    "url": "/online-quiz/static/media/bg4.7f4ca671.jpg"
+  },
+  {
+    "revision": "6a2ddad1092a0a1c326b6d0e738e682b",
+    "url": "/online-quiz/static/media/materialdesignicons-webfont.6a2ddad1.ttf"
+  },
+  {
+    "revision": "8a67040660444e8d8ee82a406d580d8f",
+    "url": "/online-quiz/static/media/materialdesignicons-webfont.8a670406.eot"
+  },
+  {
+    "revision": "c32505e8c654310ece1da4e1c10eeb57",
+    "url": "/online-quiz/static/media/materialdesignicons-webfont.c32505e8.woff"
+  },
+  {
+    "revision": "dc85ceeb0daba687e36d8dde4ed4d352",
+    "url": "/online-quiz/static/media/materialdesignicons-webfont.dc85ceeb.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.111b7a07c3c15d7fa2ab2bbdb2cf4427.js"
+  "/online-quiz/precache-manifest.d050765e366248d11ed7b2caa812b471.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/online-quiz/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
